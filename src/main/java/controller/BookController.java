6e6fb26f79f8edd10162caf1f0d1165a3f35eeec
Dilshan Herath <137473394+MadusankhaHerath@users.noreply.github.com>
2025-12-03 22:48:34 +0530

    update bookcontroller and bookserviceimpl

--- src/main/java/controller/BookController.java
+++ src/main/java/controller/BookController.java
@@ -1,4 +1,41 @@
 package controller;
 
+import dto.BookDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import service.BookService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
 public class BookController {
+
+    @Autowired
+    public BookService bookService;
+
+    @PostMapping
+    public BookDTO createBook(@RequestBody BookDTO bookDTO) {
+        return bookService.createBook(bookDTO);
+    }
+    @PutMapping("/{id}")
+    public BookDTO updateBook(@PathVariable Long id,
+                              @RequestBody BookDTO bookDTO) {
+        return bookService.updateBook(id, bookDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+    }
+
+    @GetMapping("/{id}")
+    public BookDTO getBook(@PathVariable Long id) {
+        return bookService.getBookById(id);
+    }
+
+    @GetMapping
+    public List<BookDTO> getAllBooks() {
+        return bookService.getAllBooks();
+    }
 }
--- src/main/java/service/impl/BookServiceImpl.java
+++ src/main/java/service/impl/BookServiceImpl.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import repository.BookRepository;
 import service.BookService;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class BookServiceImpl implements BookService {
 
@@ -17,6 +20,44 @@ public class BookServiceImpl implements BookService {
     @Override
     public BookDTO createBook(BookDTO bookDTO) {
         Book book = BookMapper.toEntity(bookDTO);
+        Book savedBook = bookRepository.save(book);
+        return BookMapper.toDTO(savedBook);
+    }
+    @Override
+    public BookDTO updateBook(Long id, BookDTO bookDTO) {
+        Book existingBook = bookRepository.findById(id).
+                orElseThrow(() -> new RuntimeException("Book Not Found"));
 
+        existingBook.setTitle(bookDTO.getTitle());
+        existingBook.setAuthor(bookDTO.getAuthor());
+        existingBook.setCategory(bookDTO.getCategory());
+        existingBook.setIsbn(bookDTO.getIsbn());
+        existingBook.setPublisher(bookDTO.getPublisher());
+        existingBook.setPublishedYear(bookDTO.getPublishedYear());
+        existingBook.setTotalCopies(bookDTO.getTotalCopies());
+        existingBook.setAvailableCopies(bookDTO.getAvailableCopies());
+
+        Book updated =  bookRepository.save(existingBook);
+        return BookMapper.toDTO(updated);
+    }
+
+    @Override
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    @Override
+    public BookDTO getBookById(Long id) {
+        return bookRepository.findById(id)
+                .map(BookMapper::toDTO)
+                .orElseThrow(() -> new RuntimeException("Book Not Found"));
+    }
+
+    @Override
+    public List<BookDTO> getAllBooks() {
+        return bookRepository.findAll()
+                .stream()
+                .map(BookMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
